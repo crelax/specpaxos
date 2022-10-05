931274c558003c31656799d2a0d5a0c31610a20b
Xuanhe Chen <xuanhe@comp.nus.edu.sg>
2022-10-05 14:13:16 +0800

    add runclient.sh

---
+++ bench/runclient.sh
@@ -0,0 +1,42 @@
+#!/bin/zsh -v
+
+set +x
+
+tnum=$1
+rnum=$2
+delay=0
+
+[[ $# -ne 2 && $# -ne 3 ]] && echo "USAGE: ./run.sh <tnums> <request num>" && exit 1
+
+[[ $1 -gt 0 && $1 -le 64 && $2 -gt 0 ]] || (echo "tnums in [1,64], and request num > 0" && exit 1)
+
+[[ $# -eq 3 ]] && [[ $3 -le 0 ]] && (echo "delay should be greater than 0 " && exit 1)
+
+[[ $# -eq 3 ]] && delay=$3
+
+echo "running ${tnum} cores, each send with ${rnum} requests"
+
+#dir="$(pwd)/dsarch/specpaxos"
+#bdir="${dir}/bench"
+bdir="."
+#echo ${bdir}
+
+echo "running ${1} threads, delay = ${delay}."
+
+for ((i = 0; i < $tnum; i++)); do
+  {
+    if [[ $(delay) -ne 0 ]]; then
+                      $("${bdir}/client" -t 3 -m "vr" -c "${bdir}/config.txt" -n "${rnum}" -w 30 -d "${delay}" -i ${i} )&
+      subshell=$!
+    else
+      echo no delay
+      $("${bdir}/client" -t 3 -m vr -c "${bdir}/config.txt" -n "${rnum}" -w 30 -i ${i}) &
+      subshell=$!
+    fi
+    echo "i=" ${i}
+    taskset -p --cpu-list ${i} ${subshell}
+  }
+
+done
+
+wait
